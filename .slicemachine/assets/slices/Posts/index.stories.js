2d22ad0ec7e66708db44df9aeb92b8e14673cb59
magjr <junin_15-33@hotmail.com>
2022-07-14 09:30:31 -0300

    :tada:finalized

---
+++ .slicemachine/assets/slices/Posts/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Posts';
+
+export default {
+  title: 'slices/Posts'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"posts","items":[],"primary":{"title":[{"type":"heading1","text":"Revolutionize clicks-and-mortar deliverables","spans":[]}],"description":[{"type":"paragraph","text":"Labore voluptate proident aliqua est aute nostrud duis aliqua reprehenderit deserunt nisi ad. Cupidatat velit irure aute anim aute et nostrud duis Lorem aliquip.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { GetStaticProps } from 'next';
-
 import { getPrismicClient } from '../services/prismic';
+import Header from '../components/Header';
+import Link from 'next/link'
+import {FiCalendar, FiUser} from 'react-icons/fi'
+
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,9 +27,37 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return(
+  <>
+  
+  <main  className={commonStyles.container}>
+    <Header />
+    <div className={styles.posts}>
+      <Link href='/'>
+      <a className={styles.post}>
+        <strong >Como utilizar Hooks</strong>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <ul>
+          <li>
+        <FiCalendar />
+        15 mar 2022
+          </li>
+          <li>
+        <FiUser />
+        Marco Gonzales
+          </li>
+        </ul>
+      </a>
+
+      </Link>
+    <button type='button'> Carregar mais</button>
+    </div>
+
+  </main>
+  </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
